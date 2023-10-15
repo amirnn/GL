469bd99dcfd219a4189146688f604fc8cf45f523
Amir Nourinia <amir.nouri.nia@gmail.com>
2023-10-15 08:25:34 +0200

    Use CMakePresets

--- modules/cmake/include/glad.cmake
+++ modules/cmake/include/glad.cmake
@@ -2,8 +2,6 @@
 
 add_subdirectory(glad/cmake)
 
-set(GLAD_OUT_DIR ${CMAKE_SOURCE_DIR}/lib/glad)
-
 if(WIN32)
     glad_add_library(glad_gl_core_46 STATIC LOCATION ${GLAD_OUT_DIR} API gl:core=4.6)
     set(glad_lib glad_gl_core_46)
