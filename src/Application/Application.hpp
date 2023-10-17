dbe4960790c053c6493cde7ab52a0bac40a7af35
Amir Nourinia <amir.nouri.nia@gmail.com>
2023-10-17 23:55:31 +0200

    Update Application.hpp
    Virtualise the destructor of the base class, remove terminate method from Run method

--- src/Application/Application.hpp
+++ src/Application/Application.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 class Application {
-
 public:
+  virtual ~Application() = default;
   virtual void Init() = 0;
   virtual void MainLoop() = 0;
   virtual void Terminate() = 0;
   void Run() {
+    Init();
     MainLoop();
-    Terminate();
   }
 };
