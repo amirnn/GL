1820d57dd2c9bfa0f4192cdf1a5407752811d2bd
Amir Nourinia <amir.nouri.nia@gmail.com>
2023-10-17 23:57:35 +0200

    Bogus: Still destructor for GLWindow doest not destroy GLFWWindow correctly.

--- src/Window/GLWindow.hpp
+++ src/Window/GLWindow.hpp
@@ -1,38 +1,54 @@
 #pragma once
 
 #include "Window.hpp"
+#include <assert.h>
 
 #define GLFW_INCLUDE_NONE
-#include <GLFW/glfw3.h>
 #include "glad/gl.h"
-
+#include <GLFW/glfw3.h>
 
 class GLWindow : public Window {
 
 public:
-  GLWindow(uint width, uint height, String &title, bool is_root)
-      : Window{width, height, title, is_root} {
-    m_window.reset(
-        glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL));
+  GLWindow(uint width, uint height, String &title, bool is_root,
+           GLFWwindow *wind_ptr = nullptr)
+      : Window{width, height, title, is_root}, m_window{wind_ptr} {
+    if (not wind_ptr) {
+      m_window.reset(glfwCreateWindow(m_width, m_height, m_title.c_str(),
+                                      nullptr, nullptr));
+    }
     if (not m_window) {
       throw std::runtime_error{"Window pointer not valid."};
     }
-    if (m_is_root) {
-      /* Make the window's context current */
-      glfwMakeContextCurrent(m_window.get());
+  }
+
+  //  ~GLWindow() override = default;
+  ~GLWindow() override {
+    if (m_window) {
+      m_window.reset();
     }
   }
 
-  ~GLWindow() override = default;
+  GLFWwindow *GetPointer() { return m_window.get(); }
 
-  GLFWwindow * GetPointer(){
-    return m_window.get();
+  [[nodiscard]] bool ShouldClose() const override {
+    return glfwWindowShouldClose(m_window.get());
   }
 
-  bool ShouldClose() const override { return glfwWindowShouldClose(m_window.get());}
+  [[nodiscard]] bool ShouldContinue() const {
+    return not glfwWindowShouldClose(m_window.get());
+  }
+
+  std::function<void(GLFWwindow *)> WindowResourceDestroyer{
+      [&](GLFWwindow *ptr) {
+        if (ptr) {
+          glfwDestroyWindow(ptr);
+        }
+      }};
+  using GLFWWindowPtr =
+      UPointerTD<GLFWwindow, decltype(WindowResourceDestroyer)>;
 
 private:
-  std::function<void(GLFWwindow *)> const WindowResourceDestroyer =
-      [](GLFWwindow *ptr) { glfwDestroyWindow(ptr); };
-  UPointerTD<GLFWwindow, decltype(WindowResourceDestroyer)> m_window{};
+  GLFWWindowPtr m_window{};
+  //  GLFWwindow *m_window_ptr{};
 };
